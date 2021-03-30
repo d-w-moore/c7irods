f61a1fa43ec54abd3a5af22987455e59738870b9
d-w-moore <dmoore@renci.org>
2021-03-30 16:31:12 -0400

    add runtime-only option

--- do_install.sh
+++ do_install.sh
@@ -3,10 +3,12 @@ DIR=$(dirname "$0")
 . "$DIR"/irods_setup.bash
 ensure_preinstall_pkgs 
 install_prereqs 
-db_ctl start
-db_ctl
-init_database_and_user
 set_up_package_repo 
-yum install -y irods-server 
-yum install -y irods-database-plugin-postgres.x86_64
-python /var/lib/irods/scripts/setup_irods.py < /var/lib/irods/packaging/localhost_setup_postgres.input 
+if [ $1 != "devel-and-runtime-only" ]; then
+  db_ctl start
+  db_ctl
+  init_database_and_user
+  yum install -y irods-server 
+  yum install -y irods-database-plugin-postgres.x86_64
+  python /var/lib/irods/scripts/setup_irods.py < /var/lib/irods/packaging/localhost_setup_postgres.input 
+fi
